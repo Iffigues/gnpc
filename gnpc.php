3b0a53cc802a96a7730017c0ab033a2bbcfa85d9
iffigues <iffigues@vivaldi.net>
2025-01-14 02:24:22 +0100

    ezez

---
+++ gnpc.php
@@ -0,0 +1,12 @@
+<?php
+
+function gnpc($text) {
+    $open = ["<?php", "<?=", "<?", "<%"];
+    $close = ["?>", "%>"];
+    $reg = "(<\?php=|<\?=|<\?|<%)[.]*(\?>|%>)"
+}
+
+function gnpc_from_file(string $file) {
+    $content = file_get_contents($file);
+    return gnpc($content);
+}
