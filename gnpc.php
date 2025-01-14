d151aa392f73c17e6c6cbe66246ff1bf7133132e
Iffigues <iffigues@vivaldi.net>
2025-01-14 18:00:09 +0100

    Update gnpc.php

--- gnpc.php
+++ gnpc.php
@@ -1,9 +1,17 @@
 <?php
 
-function gnpc($text) {
-    $open = ["<?php", "<?=", "<?", "<%"];
-    $close = ["?>", "%>"];
-    $reg = "<\s*(\?php|\?=|\?|%)(\s)*.*(\s)*(.)*(\?>|%>)?"
+function gnpc($text, $asp_like=false) {
+    $open = [
+        "<?php" => "?>", 
+        "<?=" => "?>",
+        "<?"=> "?>"
+    ];
+    if ($asp_like) $open["<%"] = "%>";
+
+    var_dump($open);
+    for ($i = 0; $i < strlen($text); $i++) {
+        echo $text[$i] . "\n"; // Affiche chaque caractère sur une ligne
+    }
 }
 
 function gnpc_from_file(string $file) {
