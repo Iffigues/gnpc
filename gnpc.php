e4307afdaea6dbbd8b037fca2a49d60b46183cf1
iffigues <iffigues@vivaldi.net>
2025-01-15 21:44:38 +0100

    zz

--- gnpc.php
+++ gnpc.php
@@ -1,9 +1,23 @@
 <?php
 
 function have($str, $i, $array) {
-    foreach ($array as $value) {
-        echo $value."\n";        
+    foreach ($array as $value) if (substr_compare($str, $value, $i, strlen($value)) === 0) return $value;
+    return false;
+}
+
+
+function print_to($str, $i) {
+    for (;$str[$i]; $i++) {
+        echo $str[$i];
     }
+}
+
+function parse_php($str, $i, $end) {
+    $b = $i;
+    for (; $str[$b] && substr_compare($str, $end, $b,strlen($end)) !== 0; $b++) {
+    }
+
+    return $b + strlen($end);
 }
 
 function gnpc($text, $asp_like=false) {
@@ -18,14 +32,19 @@ function gnpc($text, $asp_like=false) {
 
     for ($i = 0; $i < strlen($text); $i = $i) {
         $o = $i;
-        
+        $contain = false;
+
         if ($text[$i] == "<") 
-            $o = have($text, $i, $key);
-        else
+             $contain = have($text, $i, $key);
+        if ($contain) { 
+            $o = parse_php($text, $i, $open[$contain]); 
+        } else {
             while ($text[$o] != "<")  $o = $o + 1;
+        }
         array_push($e, substr($text, $i, $o));
         $i = $o;
     }
+    return $e;
 }
 
 function gnpc_from_file(string $file) {
@@ -33,4 +52,8 @@ function gnpc_from_file(string $file) {
     return gnpc($content);
 }
 
-gnpc_from_file("test/test1.php");
+$res = gnpc_from_file("test/test1.php");
+
+foreach($res as $k => $v) {
+    echo $k." = ".$v."\n";
+}
